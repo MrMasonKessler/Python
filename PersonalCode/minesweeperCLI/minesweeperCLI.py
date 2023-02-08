2884768c7f77c9223ddf1ba95f8a8b383fc0a2a8
Mason Kessler <mlk2927@rit.edu>
2023-02-08 12:00:09 -0500

    Starting a minesweeper project

--- PersonalCode/minesweeperCLI/minesweeperCLI.py
+++ PersonalCode/minesweeperCLI/minesweeperCLI.py
@@ -1,5 +1,28 @@
-'''
-In this file, I am going to attempt to make a minesweeper game in the command line interface.
-I will use b 
+import random
+import os
 
-'''
+
+def minesweeper():
+    clear_term()
+    size = int(input("Please enter the length of the board: "))
+    print()
+    array = [["-" for row in range(size)] for col in range(size)]
+    for row in array:
+        print(" ".join(str(cell) for cell in row))
+        print("")
+
+
+
+def clear_term(): #This function is to clear the terminal every time the game is started.
+    if os.name == 'posix':
+        os.system('clear')
+    else:
+        os.system('cls')
+
+
+def main():
+    minesweeper()
+
+
+if __name__ == "__main__":
+    main()
