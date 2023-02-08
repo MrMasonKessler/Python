488ce40c9cdb57423989f1600e56a7eca65fe1dc
Mason Kessler <mlk2927@rit.edu>
2023-02-08 10:52:33 -0500

    Added new formatting options to a string in convert.py

--- SchoolCode/convert.py
+++ SchoolCode/convert.py
@@ -1,8 +1,4 @@
 #This file is just testing basic conversions.
-
-from json.tool import main
-
-
 def convert_height():
     height_inch = int(input("Enter your height in inches: "))
     if(height_inch%12==0):
@@ -27,8 +23,6 @@ def main():
         convert_height()
     if userIn==2:
         convert_distance()
-
-
 
 if __name__ == '__main__':
     main()
