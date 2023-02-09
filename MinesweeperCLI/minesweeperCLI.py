a17eccf14d33dddc8830877b6a9a3c3d62f72564
Mason Kessler <mlk2927@rit.edu>
2023-02-09 11:45:52 -0500

    I think I finally figured out all the logic for the bomb placements.

--- MinesweeperCLI/minesweeperCLI.py
+++ MinesweeperCLI/minesweeperCLI.py
@@ -1,11 +1,31 @@
+'''
+Important note about this code: In one of my software development classes, we made a minesweeper game that was very similar in functionality to this one. 
+Some inspiration was taken from that game created during that class, such as making the bomb board before the seen board.
+Additionally, when actually printing out the map with the .join() call, I originally was doing that a much more difficult and worse way, so I ended up
+using the method that we used during class.
+'''
+
 import random
 import os
 import time
 
+#How a typical grid looks, use for reference:
 
-def minesweeper(n):
+# 0 1 2 3 4
+# 1
+# 2
+# 3
+# 4
+
+def minesweeper():
     clear_term()
-    #size = int(input("Please enter the length of the board: "))
+    choice = int(input("Please enter the what difficulty you would like to play on, 1 for easy, 2 for medium, 3 for hard: "))
+    if choice == 1:
+        size = 5
+    if choice == 2:
+        size = 10
+    if choice == 3:
+        size = 15
     print()
     print("Generating board . . .")
     time.sleep(1.5)
@@ -13,59 +33,42 @@ def minesweeper(n):
     clear_term()
     print("MINESWEEPER CLI GAME\n")
 
-
-    array = [[0 for row in range(n)] for col in range(n)] #Makes the array used in generating the board
-    x = random.randint(0,4) #Picks an x variable in the board to be a bomb
-    y = random.randint(0,4) #Picks a y variable in the board to be a bomb
+    array = [[0 for row in range(size)] for col in range(size)] #Makes the array used in generating the board
+    x = random.randint(0,size-1) #Picks an x variable in the board to be a bomb
+    y = random.randint(0,size-1) #Picks a y variable in the board to be a bomb
     array[y][x] = 'X' #Sets that selected spot to be a bomb
 
     #This next section makes it so that the spaces around the bomb are filled to be a 1, showing there is 1 bomb touching that spot
 
-    #Adds the bombs for the x-axis for columns that aren't to the far left or right
-    if (x <= 3 and x >= 1):
-       array[y][x+1] += 1
-       array[y][x-1] += 1
-    
-    #If the bomb is on far left, then the right is a bomb
-    if (x == 0):
-        array[y][x+1] += 1
+    #Check bottom first:
+    if(y >= 0 and y <= size-2):
+        if array[y+1][x] != 'X':
+            array[y+1][x] += 1 # bottom center
+    if (x >=0 and x <= size-2) and (y >= 0 and y <= size-2):
+        if array[y+1][x+1] != 'X':
+            array[y+1][x+1] += 1 # bottom right
+    if (x >= 1 and x <= size-1) and (y >= 0 and y <= size-2):
+        if array[y+1][x-1] != 'X':
+            array[y+1][x-1] += 1 # bottom left
 
-    #If the bomb is on the far right, then the left is a bomb
-    if (x == 4):
-        array[y][x-1] += 1
+    #Check top next:
+    if (x >= 0 and x <= size-1) and (y >= 1 and y <= size-1):
+        if array[y-1][x] != 'X':
+            array[y-1][x] += 1 # top center
+    if (x >= 0 and x <= size-2) and (y >= 1 and y <= size-1):
+        if array[y-1][x+1] != 'X':
+            array[y-1][x+1] += 1 # top right
+    if (x >= 1 and x <= size-1) and (y >= 1 and y <= size-1):
+        if array[y-1][x-1] != 'X':
+            array[y-1][x-1] += 1 # top left
 
-    #Checks top left of the bomb
-    if (x >= 1 and x <= 4) and (y <= 4 and y >= 1):
-        array[y-1][x-1] += 1
-
-    #Checks top right of the bomb
-    if (x >= 0 and x <= 3) and (y <= 4 and y >= 1):
-        array[y-1][x+1] += 1
-
-    #Checks the top and the bottom of the bomb
-    if (y <= 3 and y >= 1):
-        array[y-1][x] += 1
-        array[y+1][x] += 1
-    
-    #Checks the bottom of the bomb
-    if (y == 0):
-        array[y+1][x] += 1
-    
-    #Checks the top of the bomb
-    if (y == 4):
-        array[y-1][x] += 1
-
-    #Checks the bottom right of the bomb
-    if (x >= 0 and x <= 3) and (y <= 3 and y >= 0):
-        array[y+1][x+1] += 1
-
-    #Checks the bottom left of the bomb
-    if (x >= 1 and x <= 4) and (y <= 3 and y >= 0):
-        array[y+1][x-1] += 1
-
-
-
-
+    #Check sides:
+    if (x >= 1 and x <= size-1):
+        if array[y][x-1] != 'X':
+            array[y][x-1] += 1
+    if (x >= 0 and x <= size-2):
+        if array[y][x+1] != 'X':
+            array[y][x+1] += 1
 
 
     for row in array:
@@ -81,7 +84,7 @@ def clear_term(): #This function is to clear the terminal every time the game is
 
 
 def main():
-    minesweeper(5)
+    minesweeper()
 
 
 if __name__ == "__main__":
