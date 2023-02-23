4c5e62b4c7e20560d5c92ad67233846768b2923c
Mason Kessler <mlk2927@rit.edu>
2023-02-23 14:55:32 -0500

    Started using some commands from stack overflow, like sticky, for new functions I didnt know about

--- MinesweeperGUI/minesweeperGUI.py
+++ MinesweeperGUI/minesweeperGUI.py
@@ -1,3 +1,9 @@
+'''
+While I have gotten practice from the TicTacToe tkinter project, there are some commands here that I still needed to look up, and do not claim credit for.
+A great help for me was Vakus on stack overflow, as they used button commands that I didn't know about. (Stuff like sticky, lambda, and using a button list instead of making
+the buttons manually.)
+'''
+
 from tkinter import *
 from tkinter import messagebox
 import random
@@ -9,17 +15,10 @@ HEIGHT = 600
 size = 4
 bombs = 3
 board = []
+buttons = []
 root.geometry(f'{WIDTH}x{HEIGHT}')
 root.resizable(True,True)
 
-#click = True
-count = 0
-
-
-#START BY HAVING MINESWEEPER WORK AS A 4X4 GRID
-
-
-sideLen = 4 #Length of a side in button, will automate later
 
 btn1 = StringVar()
 btn2 = StringVar()
@@ -41,58 +40,7 @@ btn16 = StringVar()
 #I can add pictures for the numbers and bombs if I want, and if I add flag functionality, I will have an image for that too.
 
 def play():
-    #I can probably make a button factory function, but I will do that after confirming that I can hard code it first.)
-
-    button1 = Button(root,height=2,width=1,relief='ridge',borderwidth=.5,textvariable=btn1,command=lambda:press(1,0,0))
-    button1.grid(row=0,column=0)
-
-    button2 = Button(root,height=2,width=1,relief='ridge',borderwidth=.5,textvariable=btn2,command=lambda:press(2,0,1))
-    button2.grid(row=0,column=1)
-
-    button3 = Button(root,height=2,width=1,relief='ridge',borderwidth=.5,textvariable=btn3,command=lambda:press(3,0,2))
-    button3.grid(row=0,column=2)
-
-    button4 = Button(root,height=2,width=1,relief='ridge',borderwidth=.5,textvariable=btn4,command=lambda:press(4,0,3))
-    button4.grid(row=0,column=3)
-
-    button5 = Button(root,height=2,width=1,relief='ridge',borderwidth=.5,textvariable=btn5,command=lambda:press(5,1,0))
-    button5.grid(row=1,column=0)
-
-    button6 = Button(root,height=2,width=1,relief='ridge',borderwidth=.5,textvariable=btn6,command=lambda:press(6,1,1))
-    button6.grid(row=1,column=1)
-
-    button7 = Button(root,height=2,width=1,relief='ridge',borderwidth=.5,textvariable=btn7,command=lambda:press(7,1,2))
-    button7.grid(row=1,column=2)
-
-    button8 = Button(root,height=2,width=1,relief='ridge',borderwidth=.5,textvariable=btn8,command=lambda:press(8,1,3))
-    button8.grid(row=1,column=3)
-
-    button9 = Button(root,height=2,width=1,relief='ridge',borderwidth=.5,textvariable=btn9,command=lambda:press(9,2,0))
-    button9.grid(row=2,column=0)
-
-    button10 = Button(root,height=2,width=1,relief='ridge',borderwidth=.5,textvariable=btn10,command=lambda:press(10,2,1))
-    button10.grid(row=2,column=1)
-
-    button11 = Button(root,height=2,width=1,relief='ridge',borderwidth=.5,textvariable=btn11,command=lambda:press(11,2,2))
-    button11.grid(row=2,column=2)
-
-    button12 = Button(root,height=2,width=1,relief='ridge',borderwidth=.5,textvariable=btn12,command=lambda:press(12,2,3))
-    button12.grid(row=2,column=3)
-
-    button13 = Button(root,height=2,width=1,relief='ridge',borderwidth=.5,textvariable=btn13,command=lambda:press(13,3,0))
-    button13.grid(row=3,column=0)
-
-    button14 = Button(root,height=2,width=1,relief='ridge',borderwidth=.5,textvariable=btn14,command=lambda:press(14,3,1))
-    button14.grid(row=3,column=1)
-
-    button15 = Button(root,height=2,width=1,relief='ridge',borderwidth=.5,textvariable=btn15,command=lambda:press(15,3,2))
-    button15.grid(row=3,column=2)
-
-    button16 = Button(root,height=2,width=1,relief='ridge',borderwidth=.5,textvariable=btn16,command=lambda:press(16,3,3))
-    button16.grid(row=3,column=3)
-
-    bombBoard = makeBombBoard()
-    #print(bombBoard)
+    pass
 
 def makeBombBoard():
     #Make the field
@@ -102,59 +50,66 @@ def makeBombBoard():
         board.append([])
         for y in range(0,size):
             board[x].append(0)
-
     #Distribute bombs
-    for bomb in range(0,bombs):
+    for _ in range(0,bombs):
         x = random.randint(0,size-1)
         y = random.randint(0,size-1)
-        while board[x][y] == -1:
+        while board[x][y] == 'X':
             x = random.randint(0,size-1)
             y = random.randint(0,size-1)            
-        board[x][y] == -1
-
+        board[x][y] == 'X'
+        if(board[y][x] != 'X'):
+                board[y][x] = 'X'
+                if(y >= 0 and y <= size-2):
+                    if board[y+1][x] != 'X':
+                        board[y+1][x] += 1 # bottom center
+                if (x >=0 and x <= size-2) and (y >= 0 and y <= size-2):
+                    if board[y+1][x+1] != 'X':
+                        board[y+1][x+1] += 1 # bottom right
+                if (x >= 1 and x <= size-1) and (y >= 0 and y <= size-2):
+                    if board[y+1][x-1] != 'X':
+                        board[y+1][x-1] += 1 # bottom left
+                #Check top next:
+                if (x >= 0 and x <= size-1) and (y >= 1 and y <= size-1):
+                    if board[y-1][x] != 'X':
+                        board[y-1][x] += 1 # top center
+                if (x >= 0 and x <= size-2) and (y >= 1 and y <= size-1):
+                    if board[y-1][x+1] != 'X':
+                        board[y-1][x+1] += 1 # top right
+                if (x >= 1 and x <= size-1) and (y >= 1 and y <= size-1):
+                    if board[y-1][x-1] != 'X':
+                        board[y-1][x-1] += 1 # top left
+                #Check sides:
+                if (x >= 1 and x <= size-1):
+                    if board[y][x-1] != 'X':
+                        board[y][x-1] += 1
+                if (x >= 0 and x <= size-2):
+                    if board[y][x+1] != 'X':
+                        board[y][x+1] += 1
     return board
 
-def press(num,row,col):
-    global count
-    labelPhoto = Label(root,text='0')
-    labelPhoto.grid(row=row,column=col)
-    if num == 1:
-        btn1.set('X')
-    elif num == 2:
-        btn2.set('X')
-    elif num == 3:
-        btn3.set('X')
-    elif num == 4:
-        btn4.set('X')
-    elif num == 5:
-        btn5.set('X')
-    elif num == 6:
-        btn6.set('X')
-    elif num == 7:
-        btn7.set('X')
-    elif num == 8:
-        btn8.set('X')
-    elif num == 9:
-        btn9.set('X')
-    elif num == 10:
-        btn10.set('X')
-    elif num == 11:
-        btn11.set('X')
-    elif num == 12:
-        btn12.set('X')
-    elif num == 13:
-        btn13.set('X')
-    elif num == 14:
-        btn14.set('X')
-    elif num == 15:
-        btn15.set('X')
-    else:
-        btn16.set('X')
-    count += 1
-    checkWin()
+def makePopup():
+    global size, buttons
+    Button(root, text="Restart", command=restartGame).grid(row=0, column=0, columnspan=size, sticky=N+W+S+E)
+    buttons = []
+    for x in range(0,size):
+        buttons.append([])
+        for y in range(0,size):
+            button = Button(root,text=" ", width=2,command=lambda x=x,y=y:press(x,y))
+            buttons[x].append(button)
 
-def checkWin():
+
+def restartGame():
     pass
+
+def press(row,col):
+    global size, board, buttons
+    buttons[row][col]["text"]=str(board[row][col])
+    if board[row][col] == 'X':
+        pass
+    else:
+        pass
+
 
 def clear():
     btn1.set('')
